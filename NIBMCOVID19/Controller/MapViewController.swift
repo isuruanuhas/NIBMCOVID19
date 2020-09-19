10ae812a9aa438e7f3fb6e0186b220ba847ce6ac
Isuru Anuhas <isuruanuhasrajapaksha@gmail.com>
2020-09-20 00:21:36 +0530

    map view

---
+++ NIBMCOVID19/Controller/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  NIBMCOVID19
+//
+//  Created by isuru rajapaksha on 9/20/2563 BE.
+//  Copyright © 2563 BE RAJAPAKSHA R.W.I.A-COBSCCOMP191P039. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mymap: MKMapView!
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
