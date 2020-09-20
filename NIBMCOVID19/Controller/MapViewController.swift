db74f4d4439b6b85f0b9f5bae572c475f62055b5
Isuru Anuhas <isuruanuhasrajapaksha@gmail.com>
2020-09-20 12:50:58 +0530

    added locations

--- NIBMCOVID19/Controller/MapViewController.swift
+++ NIBMCOVID19/Controller/MapViewController.swift
@@ -14,12 +14,38 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mymap: MKMapView!
     
-  
+  let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
         
     }
     
+     override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            manager.stopUpdatingLocation()
+             render(location)
+        }
+    }
+    func render (_ location : CLLocation){
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center:coordinate , span: span)
+        mymap.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mymap.addAnnotation(pin)
+        
+    }
 }
    
