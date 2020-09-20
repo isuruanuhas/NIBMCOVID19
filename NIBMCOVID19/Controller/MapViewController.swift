ddeb8687f9c9c663003917295c4778f58a0c181c
Isuru Anuhas <isuruanuhasrajapaksha@gmail.com>
2020-09-20 11:53:16 +0530

    initialise map

--- NIBMCOVID19/Controller/MapViewController.swift
+++ NIBMCOVID19/Controller/MapViewController.swift
@@ -8,27 +8,18 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mymap: MKMapView!
     
   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+      
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+   
